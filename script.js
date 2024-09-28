d6a86e15d9f000ff7c5046a8681dc83f92f85fd7
Zioxs- <119772194+Zioxs@users.noreply.github.com>
2024-09-28 21:36:57 +0700

    "Deleted index.html, script.js, and styles.css files"

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add event listener to navbar items
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-    });
-});
